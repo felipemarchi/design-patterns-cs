3fa1152af69d482d98fa899425ffeeaec2b29d19
LucasCancio <lucas.cancio7@gmail.com>
2021-01-13 12:02:54 -0300

    Prototype

---
+++ PatternsCriacao/Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncionarioPermanente func_permanente = new FuncionarioPermanente
+            {
+                Nome = "João",
+                Idade = 30,
+                Tipo = "Permanente"
+            };
+
+            FuncionarioPermanente clone_func_permanente = (FuncionarioPermanente)func_permanente.Clone();
+            clone_func_permanente.Nome = "Carlos";
+            clone_func_permanente.Idade = 40;
+
+            Console.WriteLine("Detalhes do funcionário Permanente:");
+            Console.WriteLine($"Nome: {func_permanente.Nome} \nIdade: {func_permanente.Idade} \nTipo: {func_permanente.Tipo}");
+
+            Console.WriteLine("\nDetalhes do funcionário Permanente Clonado:");
+            Console.WriteLine($"Nome: {clone_func_permanente.Nome} \nIdade: {clone_func_permanente.Idade} \nTipo: {clone_func_permanente.Tipo}");
+        }
+    }
+}
