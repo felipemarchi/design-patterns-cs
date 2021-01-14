8ed8a29aec02dc4dc40318c63f74b6c4f79b911a
LucasCancio <lucas.cancio7@gmail.com>
2021-01-13 21:50:51 -0300

    Facade

---
+++ PatternsEstruturais/Facade/Facade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class Facade
+    {
+        private SubSistemaUm um;
+
+        private SubSistemaDois dois;
+
+        private SubSistemaTres tres;
+
+        public Facade()
+        {
+            this.um = new SubSistemaUm();
+            this.dois = new SubSistemaDois();
+            this.tres = new SubSistemaTres();
+        }
+
+        public void MetodoA()
+        {
+            Console.WriteLine("\nMétodoA() -----");
+            this.dois.MetodoDois();
+            this.tres.MetodoTres();
+        }
+
+        public void MetodoB()
+        {
+            Console.WriteLine("\nMétodoB() -----");
+            this.um.MetodoUm();
+            this.tres.MetodoTres();
+        }
+    }
+}
